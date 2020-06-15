974bf23f855972679adafa420994575b91374b24
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-06-15 09:03:35 -0700

    Prettier Formatting

--- app.ts
+++ app.ts
@@ -1,22 +1,30 @@
 // API (ABC Module - Express)
-import { Application, Context } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
-import {get_all_books,get_book } from "./controllers/bookController.ts"
+import {
+  Application,
+  Context,
+} from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
+
+import {
+  get_all_books,
+  get_book,
+  create_book,
+  delete_book,
+} from "./controllers/bookController.ts";
 const app = new Application();
 
-
 // Static Files (Block by default for security reasons)
-app.static('/','./public');
+app.static("/", "./public");
 
 // Routes
-app.get('/', async (ctx: Context) => {
-	await ctx.file('./public/index.html')
-})
+app.get("/", async (ctx: Context) => {
+  await ctx.file("./public/index.html");
+});
 
 app
-.get('/books', get_all_books)
-.get('/book/:id', get_book)
-.post('/books', () => {})
-.delete('/books/:id', () => {})
+  .get("/books", get_all_books)
+  .get("/book/:id", get_book)
+  .post("/books", create_book)
+  .delete("/books/:id", delete_book);
 
 // Listen to port
-app.start({port: 3000});
+app.start({ port: 3000 });
