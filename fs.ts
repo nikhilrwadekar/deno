6ce7cbf43517f2ec3df7e46a4582fc96f30f7653
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-06-15 08:11:10 -0700

    Added writeJson

--- fs.ts
+++ fs.ts
@@ -3,3 +3,11 @@ import { readJson, writeJson } from "https://deno.land/std/fs/mod.ts"; //Unstabl
 
 const jsonObj = await readJson('random.json');
 console.log(jsonObj);
+
+const books = [
+	{title: "Title One", author: "Nikhil Wadekar"},
+	{title: "Name of the Wind", author: "Shaun"}
+]
+
+await writeJson('books.json', books, {spaces: 2});
+console.log("Created books.json")
